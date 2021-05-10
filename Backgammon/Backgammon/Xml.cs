dc47f9c0d8a8462942c586ebef2fab171a94f071
EugeneZozulya <y.zozulia@student.csn.khai.edu>
2021-05-10 10:20:12 +0300

    Slave commit

--- Backgammon/Backgammon/Xml.cs
+++ Backgammon/Backgammon/Xml.cs
@@ -1,26 +1,58 @@
-﻿using System;
-using System.IO;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System.Xml.Serialization;
 
 namespace Backgammon
 {
+    /// <summary>
+    /// Save/download game in the xml file.
+    /// </summary>
     static class XML
     {
+        /// <summary>
+        /// Save game.
+        /// </summary>
+        /// <param name="fileName"> File name. </param>
+        /// <param name="game"> Game. </param>
         static public void Save(string fileName, GameController game)
         {
+            if (!Directory.Exists("Save")) Directory.CreateDirectory("Save");
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(GameController));
+            using (FileStream stream = new FileStream(@"Save\" + fileName, FileMode.Create))
+            {
+                xmlSerializer.Serialize(stream, game);
+            }
         }
+        /// <summary>
+        /// Download game.
+        /// </summary>
+        /// <param name="fileName"> File name. </param>
+        /// <returns> Game. </returns>
         static public GameController Download(string fileName)
         {
             GameController game = null;
+            if (Directory.Exists("Save"))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(GameController));
+                using (FileStream stream = new FileStream(@"Save\" + fileName, FileMode.Create))
+                {
+                    game = (GameController)xmlSerializer.Deserialize(stream);
+                }
+            }
             return game;
         } 
-        static public (List<string>, List<string>) SearchSave()
+        /// <summary>
+        /// Search save game on the computer.
+        /// </summary>
+        /// <returns> Files name and files info. </returns>
+        static public (string[], string[]) SearchSave()
         {
-            List<string> saves = new List<string>();
-            List<string> info = new List<string>();
-            return (saves,info);
+            string[] filesName = Directory.GetFiles("Save");
+            string[] filesInfo = new string[filesName.Length];
+            for(int i = 0; i<filesName.Length; i++)
+            {
+                filesInfo[i] = File.GetCreationTime(filesName[i]).ToLongDateString();
+            }
+            return (filesName,filesInfo);
         }
     }
 }
